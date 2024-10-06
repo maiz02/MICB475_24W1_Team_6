48b85d9e5545ac9f0885b1425ffe83426f5bb19b
JuliaJung00 <130949830+JuliaJung00@users.noreply.github.com>
2024-10-06 16:55:51 -0700

    Update Data_Wrangling.R
    filtered out the NA values & MS patient samples

--- R_Code/Data_Wrangling.R
+++ R_Code/Data_Wrangling.R
@@ -19,3 +19,22 @@ site_country <- ifelse(x %in% c("San Francisco", "Boston", "New York", "Pittsbur
                              ifelse(x == "Buenos Aires", "Argentina", 
                                     ifelse(x == "San Sebastian", "Spain", x))))
 msmeta_col$site_x <- site_country
+
+# dropping samples with NA values
+msmeta_col <- msmeta_col %>%
+  drop_na(asthma, allergies)
+
+#filtering healthy individuals
+msmeta_col <- msmeta_col %>%
+  filter(disease != "MS")
+
+#generating filtered ms metadata set with the remaining columns
+filtered_msmeta <- msmeta_col %>%
+  left_join(msmeta, by = "sample-id")
+
+filtered_msmeta <- filtered_msmeta %>%
+  rename("ms_status" = "disease.x", "country" = "site_x.x", "allergies" = "allergies.x", "asthma" = "asthma.x", "region" = "site_x.y")
+
+# exporting the filtered_msmeta to a TSV file
+output_filepath <- "filtered_ms_metadata.tsv"
+write_tsv(filtered_msmeta, output_filepath)
