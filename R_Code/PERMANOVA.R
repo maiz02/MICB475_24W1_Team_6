0e15150c7ed594fe091f77098e5ad0e67b283f84
chaeyoonchang <cchang39@student.ubc.ca>
2024-11-18 12:12:38 -0800

    updated code

--- R_Code/PERMANOVA.R
+++ R_Code/PERMANOVA.R
@@ -5,13 +5,14 @@ dm_unifrac <- UniFrac(ms_rare, weighted=TRUE) # Weighted UniFrac
 dm_braycurtis <- vegdist(t(otu_table(ms_rare)), method="bray") # Bray-curtis
 dm_jaccard <- vegdist(t(otu_table(ms_rare)), method="jaccard") # Jaccard
 
-df <- as.data.frame(sample_data(ms_rare))
+df <- as.data.frame(as.matrix(sample_data(ms_rare)))
 
+#allergies
 adonis2(dm_unifrac ~ upf_status * allergies, data =df)
 adonis2(dm_braycurtis ~ upf_status*allergies, data =df)
 adonis2(dm_jaccard ~ upf_status*allergies, data =df)
 
-
+#asthma
 adonis2(dm_unifrac ~ upf_status * asthma, data =df)
 adonis2(dm_braycurtis ~ upf_status*asthma, data =df)
 adonis2(dm_jaccard ~ upf_status*asthma, data =df)
