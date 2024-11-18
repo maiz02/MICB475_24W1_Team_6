4cb38e5c6943cc17c3399e31fa382a8dd953f363
chaeyoonchang <cchang39@student.ubc.ca>
2024-11-18 12:20:40 -0800

    permanova results

--- R_Code/PERMANOVA.R
+++ R_Code/PERMANOVA.R
@@ -8,11 +8,20 @@ dm_jaccard <- vegdist(t(otu_table(ms_rare)), method="jaccard") # Jaccard
 df <- as.data.frame(as.matrix(sample_data(ms_rare)))
 
 #allergies
-adonis2(dm_unifrac ~ upf_status * allergies, data =df)
-adonis2(dm_braycurtis ~ upf_status*allergies, data =df)
-adonis2(dm_jaccard ~ upf_status*allergies, data =df)
+permanova_wuni_allergies <- adonis2(dm_unifrac ~ upf_status * allergies, data =df)
+permanova_bray_allergies <- adonis2(dm_braycurtis ~ upf_status*allergies, data =df)
+permanova_jacc_allergies <- adonis2(dm_jaccard ~ upf_status*allergies, data =df)
 
 #asthma
-adonis2(dm_unifrac ~ upf_status * asthma, data =df)
-adonis2(dm_braycurtis ~ upf_status*asthma, data =df)
-adonis2(dm_jaccard ~ upf_status*asthma, data =df)
+permanova_wuni_asthma <- adonis2(dm_unifrac ~ upf_status * asthma, data =df)
+permanova_bray_asthma <- adonis2(dm_braycurtis ~ upf_status*asthma, data =df)
+permanova_jacc_asthma <- adonis2(dm_jaccard ~ upf_status*asthma, data =df)
+
+# Save the results as a CSV file
+write.csv(permanova_wuni_allergies, "R_Files/permanova_wuni_allergies.csv", row.names = TRUE)
+write.csv(permanova_bray_allergies, "R_Files/permanova_bray_allergies.csv", row.names = TRUE)
+write.csv(permanova_jacc_allergies, "R_Files/permanova_jacc_allergies.csv", row.names = TRUE)
+write.csv(permanova_wuni_asthma, "R_Files/permanova_wuni_asthma.csv", row.names = TRUE)
+write.csv(permanova_bray_asthma, "R_Files/permanova_bray_asthma.csv", row.names = TRUE)
+write.csv(permanova_jacc_asthma, "R_Files/permanova_wuni_asthma.csv", row.names = TRUE)
+
