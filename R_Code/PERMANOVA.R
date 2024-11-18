4efa4328bec83d111499aea10007c800ab5a1d3a
chaeyoonchang <cchang39@student.ubc.ca>
2024-11-18 11:59:47 -0800

    permanova script

---
+++ R_Code/PERMANOVA.R
@@ -0,0 +1,12 @@
+library(vegan)
+library(phyloseq)
+
+dm_unifrac <- UniFrac(ms_rare, weighted=TRUE) # Weighted UniFrac
+dm_braycurtis <- vegdist(t(otu_table(ms_rare)), method="bray") # Bray-curtis
+dm_jaccard <- vegdist(t(otu_table(ms_rare)), method="jaccard") # Jaccard
+
+df <- as.data.frame(sample_data(ms_rare))
+
+adonis2(dm_unifrac ~ upf_status * allergies, data =df)
+adonis2(dm_braycurtis ~ upf_status*allergies, data =df)
+adonis2(dm_jaccard ~ upf_status*allergies, data =df)
