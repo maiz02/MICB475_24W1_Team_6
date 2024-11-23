13474091577900a1ba090d75847793497d5e78db
JuliaJung00 <dayejung0315@gmail.com>
2024-11-22 18:22:47 -0800

    updated codes

--- R_Code/phyloseq.R
+++ R_Code/phyloseq.R
@@ -81,7 +81,7 @@ sample_data(upf_phyloseq_low)
 tax_table(upf_phyloseq_low)
 phy_tree(upf_phyloseq_low)
 
-#### Accessor functions ####
+#### Accessor functions #### UPF HIGH
 # These functions allow you to see or summarise data
 
 # If we look at sample variables and decide we only want some variables, we can view them like so:
@@ -134,4 +134,61 @@ upf_phyloseq_rare_high <- rarefy_even_depth(upf_phyloseq_final_high, rngseed = 1
 ##### Saving #####
 save(upf_phyloseq_final_high, file="R_Code/upf_phyloseq_final_high.RData")
 save(upf_phyloseq_rare_high, file="R_Code/upf_phyloseq_rare_high.RData")
+
+
+
+
+#### Accessor functions #### UPF LOW
+# These functions allow you to see or summarise data
+
+# If we look at sample variables and decide we only want some variables, we can view them like so:
+sample_variables(upf_phyloseq_low)
+# colnames(sample_data(atamaca))
+get_variable(upf_phyloseq_low, c("upf_status","asthma")) # equivalent to "select" in tidyverse
+
+## Let's say we want to filter OTU table by sample. 
+# We can first view sample names:
+sample_names(upf_phyloseq_low)
+# How many samples do we have?
+nsamples(upf_phyloseq_low) #425
+# What is the sum of reads in each sample?
+sample_sums(upf_phyloseq_low)
+# Save the sample names of the 3 samples with the most reads
+getsamps_low <- names(sort(sample_sums(upf_phyloseq_low), decreasing = TRUE)[1:3])
+# filter to see taxa abundances for each sample
+get_taxa(upf_phyloseq_low, getsamps_low) 
+
+## Conversely, let's say we want to compare OTU abundance of most abundant OTU across samples
+# Look at taxa names
+taxa_names(upf_phyloseq_low)
+# How many taxa do we have?
+ntaxa(upf_phyloseq_low)
+# What is the total read count for each taxa?
+taxa_sums(upf_phyloseq_low)
+# Let's find the top 3 most abundant asvs (taxa) in our data
+gettaxa_low <- names(sort(taxa_sums(upf_phyloseq_low), decreasing = TRUE)[1:3] )
+get_sample(upf_phyloseq_low, gettaxa_low)
+
+
+######### ANALYZE ##########
+# Remove non-bacterial sequences, if any
+upf_phyloseq_filt_low <- subset_taxa(upf_phyloseq_low,  Domain == "d__Bacteria" & Class!="c__Chloroplast" & Family !="f__Mitochondria")
+# Remove ASVs that have less than 5 counts total
+upf_phyloseq_filt_nolow_low <- filter_taxa(upf_phyloseq_filt_low, function(x) sum(x)>5, prune = TRUE)
+# Remove samples with less than 100 reads
+upf_phyloseq_nolow_samps_low <- prune_samples(sample_sums(upf_phyloseq_filt_nolow_low)>100, upf_phyloseq_filt_nolow_low)
+# Remove samples where upf_status is na
+upf_phyloseq_final_low <- subset_samples(upf_phyloseq_nolow_samps_low, !is.na(upf_status) )
+
+# Rarefy samples
+# rngseed sets a random number. If you want to reproduce this exact analysis, you need
+# to set rngseed the same number each time
+# t transposes the table to use rarecurve function
+# cex decreases font size
+rarecurve(t(as.data.frame(otu_table(upf_phyloseq_final_low))), cex=0.1)
+upf_phyloseq_rare_low <- rarefy_even_depth(upf_phyloseq_final_low, rngseed = 2, sample.size = 1000)
+
+##### Saving #####
+save(upf_phyloseq_final_low, file="R_Code/upf_phyloseq_final_low.RData")
+save(upf_phyloseq_rare_low, file="R_Code/upf_phyloseq_rare_low.RData")
 
