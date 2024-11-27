25b752e26face9fbfc2f90fc0c591440dbe4b73a
JuliaJung00 <dayejung0315@gmail.com>
2024-11-26 21:24:11 -0800

    uploaded merged alpha diversity plots

--- R_Code/AlphaDiversity_Asthma.R
+++ R_Code/AlphaDiversity_Asthma.R
@@ -1,6 +1,9 @@
+install.packages("patchwork")
+
 library(phyloseq)
 library(tidyverse)
 library(picante)
+library(patchwork)
 
 load("R_Code/upf_phyloseq_rare_high.RData")
 load("R_Code/upf_phyloseq_rare_low.RData")
@@ -8,9 +11,11 @@ load("R_Code/upf_phyloseq_rare_low.RData")
 #### HIGH UPF ####
 # Observed Features and Shannon Diversity
 gg_richness_high <- plot_richness(upf_phyloseq_rare_high, x = "upf_asthma", 
-                             measures = c("Observed","Shannon")) +
-  xlab("High UPF and Asthma") +
-  geom_boxplot()
+                                  measures = c("Observed","Shannon")) +
+  xlab(" ") +
+  scale_x_discrete(labels = c("No Asthma", "Asthma")) +
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 0, hjust = 0.5))  
 gg_richness_high
 
 ggsave(filename = "R_Files/gg_richness_high.png", 
@@ -24,21 +29,40 @@ phylo_dist_high <- pd(t(otu_table(upf_phyloseq_rare_high)), phy_tree(upf_phylose
 sample_data(upf_phyloseq_rare_high)$PD <- phylo_dist_high$PD
 
 pd_plot_high <- ggplot(sample_data(upf_phyloseq_rare_high), aes(upf_asthma, PD)) + 
+  geom_point(position = position_dodge(width = 1), size = 1.5, alpha = 1) +
   geom_boxplot() +
-  xlab("High UPF and Asthma") +
-  ylab("Phylogenetic Diversity")
+  xlab(" ") +
+  scale_x_discrete(labels = c("No Asthma", "Asthma")) +  # Label adjustment
+  ylab("Phylogenetic Diversity") +
+  theme(axis.text.x = element_text(size = 9), axis.text.y = element_text(size = 9),
+        axis.title.x = element_text(size = 11), axis.title.y = element_text(size = 11))+
+  scale_colour_manual(name="", values=c(Com = "lightgrey") )
+
+pd_plot_high<- pd_plot_high+facet_grid(. ~ "Faith's PD") +
+  theme(strip.background = element_rect(fill="lightgrey"),
+        strip.text = element_text(size=8, colour="black"))
 pd_plot_high
+
 
 ggsave(filename = "R_Files/pd_plot_high.png", 
        pd_plot_high,
-       height=4, width=6)
+       height=4, width=3)
+
+
+combined_plot <- gg_richness_high + pd_plot_high + 
+  plot_layout(ncol = 2, widths = c(2, 1))
+ggsave(filename = "R_Files/combined_high_upf.png", 
+       combined_plot, height = 4, width = 9)
+
 
 #### LOW UPF ####
 # Observed Features and Shannon Diversity
 gg_richness_low <- plot_richness(upf_phyloseq_rare_low, x = "upf_asthma", 
-                             measures = c("Observed","Shannon")) +
-  xlab("Low UPF and Asthma") +
-  geom_boxplot()
+                                  measures = c("Observed","Shannon")) +
+  xlab(" ") +
+  scale_x_discrete(labels = c("No Asthma", "Asthma")) +
+  geom_boxplot() +
+  theme(axis.text.x = element_text(angle = 0, hjust = 0.5))  
 gg_richness_low
 
 ggsave(filename = "R_Files/gg_richness_low.png", 
@@ -47,16 +71,32 @@ ggsave(filename = "R_Files/gg_richness_low.png",
 
 # Faith's PD
 phylo_dist_low <- pd(t(otu_table(upf_phyloseq_rare_low)), phy_tree(upf_phyloseq_rare_low),
-                 include.root=F) 
+                      include.root=F) 
 
 sample_data(upf_phyloseq_rare_low)$PD <- phylo_dist_low$PD
 
 pd_plot_low <- ggplot(sample_data(upf_phyloseq_rare_low), aes(upf_asthma, PD)) + 
+  geom_point(position = position_dodge(width = 1), size = 1.5, alpha = 1) +
   geom_boxplot() +
-  xlab("Low UPF and Asthma") +
-  ylab("Phylogenetic Diversity")
+  xlab(" ") +
+  scale_x_discrete(labels = c("No Asthma", "Asthma")) +  # Label adjustment
+  ylab("Phylogenetic Diversity") +
+  theme(axis.text.x = element_text(size = 9), axis.text.y = element_text(size = 9),
+        axis.title.x = element_text(size = 11), axis.title.y = element_text(size = 11))+
+  scale_colour_manual(name="", values=c(Com = "lightgrey") )
+
+pd_plot_low<- pd_plot_low+facet_grid(. ~ "Faith's PD") +
+  theme(strip.background = element_rect(fill="lightgrey"),
+        strip.text = element_text(size=8, colour="black"))
 pd_plot_low
+
 
 ggsave(filename = "R_Files/pd_plot_low.png", 
        pd_plot_low,
-       height=4, width=6)
+       height=4, width=3)
+
+
+combined_plot <- gg_richness_low + pd_plot_low + 
+  plot_layout(ncol = 2, widths = c(2, 1))
+ggsave(filename = "R_Files/combined_low_upf.png", 
+       combined_plot, height = 4, width = 9)
