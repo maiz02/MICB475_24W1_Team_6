5aec1557f545603d09345e96fabae6efe00205ad
chaeyoonchang <cchang39@student.ubc.ca>
2024-12-10 01:02:15 -0800

    Core microbiome prevalence adjusted, white background added, code edited

--- R_Code/CoreMicrobiome_asthma_low_upf.R
+++ R_Code/CoreMicrobiome_asthma_low_upf.R
@@ -22,24 +22,11 @@ upf_low_RA <- transform_sample_counts(upf_phyloseq_final_low, fun=function(x) x/
 upf_low_asthma <- subset_samples(upf_low_RA, `asthma`==1)
 upf_low_noasthma <- subset_samples(upf_low_RA, `asthma`==0)
 
-# What ASVs are found in more than 70% of samples in each antibiotic usage category?
-# trying changing the prevalence to see what happens
-upf_low_asthma_ASVs <- core_members(upf_low_asthma, detection=0, prevalence = 0.7)
-upf_low_noasthma_ASVs <- core_members(upf_low_noasthma, detection=0, prevalence = 0.7)
-
-# What are these ASVs? you can code it in two different ways to see the same things
-tax_table(prune_taxa(upf_low_asthma_ASVs,upf_phyloseq_final_low))
-tax_table(prune_taxa(upf_low_noasthma_ASVs,upf_phyloseq_final_low))
-
-# can plot those ASVs' relative abundance
-prune_taxa(upf_low_asthma_ASVs,upf_low_RA) %>% 
-  plot_bar(fill="Genus") + 
-  facet_wrap(.~`asthma`, scales ="free")
 
 # Notice that in this dataset, there are very few CORE microbiome members. This is common
 ### What if we wanted to make a Venn diagram of all the ASVs that showed up in each treatment?
-upf_low_asthma_list <- core_members(upf_low_asthma, detection=0.001, prevalence = 0.10)
-upf_low_noasthma_list <- core_members(upf_low_noasthma, detection=0.001, prevalence = 0.10)
+upf_low_asthma_list <- core_members(upf_low_asthma, detection=0.001, prevalence = 0.50)
+upf_low_noasthma_list <- core_members(upf_low_noasthma, detection=0.001, prevalence = 0.50)
 
 upf_low_asthma_list_full <- list(Asthma = upf_low_asthma_list, No_Asthma = upf_low_noasthma_list)
 
@@ -56,9 +43,16 @@ library("sf")
 ## MAC USERS BEWARE ENDS HERE##
 
 # Create a Venn diagram using all the ASVs shared and unique to asthma and non-asthma
-low_venn <- ggVennDiagram(x = upf_low_asthma_list_full, label_alpha = 1.0,
-                           label_size = 5,        
-                           edge_size = 0.5) + coord_cartesian(clip = "off") + coord_fixed(ratio = 0.55)
+low_venn <- ggVennDiagram(x = upf_low_asthma_list_full, 
+                          label_alpha = 1.0,
+                           label_size = 7,        
+                           edge_size = 0.5) + 
+  coord_cartesian(clip = "off") + 
+  coord_fixed(ratio = 0.75) +
+  labs(caption = "Low UPF") +
+  theme(plot.caption = element_text(hjust = 0.5, size = 14, face = "bold", margin = margin(t = 10)),
+        plot.background = element_rect(fill = "white", color = NA))
+
 
 ggsave("R_files/venn_asthma_low_upf.png", plot = low_venn, width = 12, height = 7, dpi = 300)
 
